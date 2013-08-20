44a50f2377466b26e7b23b36e402f9655f5f7cf0
Mitchell <mbruckner807@gmail.com>
2013-08-19 19:39:37 -0500

    first commit

---
+++ livia_client.py
@@ -0,0 +1,10 @@
+import urllib
+import json
+import time
+
+def log(url,loggerid,data,timestamp=None):
+    if timestamp is None :
+        timestamp = int(time.time())
+    outdata = {"logger":loggerid,"data":data,"timestamp":timestamp}
+    outstring = json.dumps(outdata)
+    urllib.request.urlopen(url,data=outstring)
