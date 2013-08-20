59983f7afd1e53272c3628732a94d5cc5edf796c
Mitchell <mbruckner807@gmail.com>
2013-08-19 21:40:52 -0500

    another commit

--- livia_client.py
+++ livia_client.py
@@ -1,10 +1,21 @@
 import urllib.request
+import urllib.parse
 import json
 import time
 
-def log(url,loggerid,data,timestamp=None):
+class logger():
+    url=None
+    number=None
+    def __init__(self,url):
+        self.url=url
+        response=urllib.request.urlopen(url,data=urllib.parse.urlencode({"authentication":""}).encode())
+        number = int(response.read())
+        #connect and get id here
+        pass
+
+def log(logger,data,timestamp=None):
     if timestamp is None :
         timestamp = int(time.time())
-    outdata = {"logger":loggerid,"data":data,"timestamp":timestamp}
+    outdata = {"logger":logger.number,"data":data,"timestamp":timestamp}
     outstring = json.dumps(outdata)
-    urllib.request.urlopen(url,data=outstring.encode())
+    urllib.request.urlopen(url,data=urllib.parse.urlencode({"entry":outstring}).encode())
