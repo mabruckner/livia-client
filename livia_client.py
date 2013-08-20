268896303cb58e4db5672e77dc454addbc2db936
Mitchell <mbruckner807@gmail.com>
2013-08-19 22:48:08 -0500

    more stuff

--- livia_client.py
+++ livia_client.py
@@ -4,18 +4,15 @@ import json
 import time
 
 class logger():
-    url=None
-    number=None
-    datatype=None
     def __init__(self,url,datatype):
         self.url=url
-        response=urllib.request.urlopen(url,data=urllib.parse.urlencode({"datatype":datatype}).encode())
-        number = int(response.read())
+        self.response=urllib.request.urlopen(url,data=urllib.parse.urlencode({"datatype":datatype}).encode())
+        self.number = int(response.read())
         #connect and get id here
 
-    def log(self,data,timestamp=None):
+    def log(self,data,datatype="",timestamp=None):
         if timestamp is None :
             timestamp = int(time.time())
-        outdata = {"logger":self.number,"data":data,"timestamp":timestamp}
+        outdata = {"logger":self.number,"datatype":datatype,data":data,"timestamp":timestamp}
         outstring = json.dumps(outdata)
         urllib.request.urlopen(self.url,data=urllib.parse.urlencode({"entry":outstring}).encode())
