ef0772daaf5298644095ac4e29f8ea64bec60e6f
Mitchell <mbruckner807@gmail.com>
2013-08-22 14:05:42 -0500

    I changed everything

--- livia.py
+++ livia.py
@@ -4,19 +4,29 @@ import json
 import time
 
 class logger():
-    def __init__(self,url,project,id=None):
+    def __init__(self,url,project,id,key):
         self.url=url
         self.project = project
-        if id is not None :
-            self.number = id
-        else :
-            print("querying ",url+"/projects/"+project+"/addlogger")
-            response=urllib.request.urlopen(url+"/projects/"+project+"/addlogger")
-            self.number = int(response.read())
+        self.key = key
+        self.number = id
 
-    def log(self,data,datatype="",timestamp=None):
+    def log(self,data,timestamp=None):
         if timestamp is None :
             timestamp = int(time.time())
-        outdata = {"logger":self.number,"datatype":datatype,"data":data,"timestamp":timestamp}
+        outdata = {"logger":self.number,"data":data,"timestamp":timestamp}
         outstring = json.dumps(outdata)
-        urllib.request.urlopen(self.url+"/projects/"+self.project,data=urllib.parse.urlencode({"entry":outstring}).encode())
+        urllib.request.urlopen(self.url+"/projects/"+self.project+"/",data=urllib.parse.urlencode({"entry":outstring,"key":self.key}).encode())
+    def save(self,fname):
+        fp=open(fname,"w")
+        json.dump({"id":self.number,"url":self.url,"project":self.project,"key":self.key},fp)
+    def load(self,fname):
+        fp=open(fname)
+        dat=json.load(fp)
+        self.number=dat["id"]
+        self.url=dat["url"]
+        self.project=dat["project"]
+        self.key=dat["key"]
+    def load(fname):
+        fp=open(fname)
+        dat=json.load(fp)
+        return logger(dat["url"],dat["project"],dat["id"],dat["key"])
